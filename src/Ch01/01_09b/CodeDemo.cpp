40c317c927e3bbbdee19f034fe6648854d4ce71b
Shreyas <43398233+foxtrotsk@users.noreply.github.com>
2025-03-30 06:45:06 +0000

    Terminal interaction

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -1,12 +1,25 @@
-// Complete Guide to C++ Programming Foundations
-// Challenge 01_09
-// Terminal Interaction, by Eduardo Corpeño 
+#include<iostream>
 
-#include <iostream>
+using std::endl;
+using std::cout;
+using std::cin;
+using std::string;
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    string name;
+    // char name [15];
 
-    std::cout << std::endl << std::endl;
+    cout << "Enter your name: " << endl;
+    // cin will stop at newline / space character
+    // to get name with spaces use getline
+    // cin >> name;
+    // cin.getline(name, 15);
+
+    // If you are using string
+    // You will have to use 
+    // getline(cin, name) instead of cin.getline(name, 15)
+    getline(cin, name);
+    cout << "Hi " << name << " , How are you?" << endl;
+    cout << endl << endl;
     return 0;
 }
